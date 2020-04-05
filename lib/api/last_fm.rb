bb0f394d0174015f85a437ab874d89aa0b4c7fef
thetinahang <thetinahang@gmail.com>
2020-04-05 15:21:27 -0700

    edit url to get a result

--- lib/api/last_fm.rb
+++ lib/api/last_fm.rb
@@ -13,21 +13,14 @@ module Api
     end
 
     def get_response
-      response = HTTParty.get(build_auth_url)
+      response = HTTParty.get(lastfm_url)
       JSON.parse(response.body) if response.present?
     end
 
-    def build_auth_url
-      @auth_url ||= lastfm_auth_url + api_key
+    def lastfm_url
+      "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=cher&api_key=#{ENV['LASTFM_API_KEY']}&format=json"
     end
 
-    def lastfm_auth_url
-      'http://www.last.fm/api/auth/?api_key='
-    end
-
-    def api_key
-        ENV['LASTFM_API_KEY']
-    end  
   end
 
 end
