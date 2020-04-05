1329aaf5ae93e6a1ad43c776aa78551820d589a6
thetinahang <thetinahang@gmail.com>
2020-04-05 13:57:32 -0700

    build api methods

---
+++ app/models/last_fm.rb
@@ -0,0 +1,22 @@
+class LastFm
+  include HTTParty
+
+  def get_response
+    response = HTTParty.get(build_auth_url)
+    response.body
+  end
+
+  private
+
+  def build_auth_url
+    @auth_url ||= lastfm_auth_url + api_key
+  end
+
+  def lastfm_auth_url
+    'http://www.last.fm/api/auth/?api_key='
+  end
+
+  def api_key
+      ENV[“LASTFM_API_KEY”]
+  end  
+end
