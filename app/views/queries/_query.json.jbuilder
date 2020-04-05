abdb8bf736fa0cb79ca7d27088bf5e0497070ff1
thetinahang <thetinahang@gmail.com>
2020-04-04 20:12:28 -0700

    create scaffold for query

---
+++ app/views/queries/_query.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! query, :id, :name, :created_at, :updated_at
+json.url query_url(query, format: :json)
---
+++ db/migrate/20200405031045_create_queries.rb
@@ -0,0 +1,9 @@
+class CreateQueries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :queries do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
