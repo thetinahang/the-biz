1c997f430d91175937ba28f59e1d479965691373
thetinahang <thetinahang@gmail.com>
2020-04-26 12:52:04 -0700

    remove methods that require individual user credentials

--- app/models/term.rb
+++ app/models/term.rb
@@ -4,14 +4,12 @@ class Term < ApplicationRecord
   TYPES = %w[
     artist.getSimilar
     artist.getInfo
-    artist.getTags
     artist.getTopAlbums
     artist.getTopTags
     artist.getTopTracks
     artist.search
     track.getInfo
     track.getSimilar
-    track.getTags
     track.getTopTags
     track.search
   ].freeze
